dc584979f07e9a6146622bcb182ca8862e641481
turtleman270 <ahill5067@gmail.com>
2019-05-21 21:26:27 -0400

    replace bar graph with table

--- src/components/Leaderboard.vue
+++ src/components/Leaderboard.vue
@@ -1,15 +1,28 @@
 <template>
   <div class="col">
-    <div :id="ruleset + '-bar'" style="min-width: 310px; height: 400px;"></div>
     <div :id="ruleset + '-line'" style="min-width: 310px; height: 400px;"></div>
+
+    <table class="table table-striped table-bordered">
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Elo</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="({ person, elo }, rank) in infos" :key="person">
+          <td>{{ rank + 1 }}. {{ person }}</td>
+          <td>{{ elo }}</td>
+        </tr>
+      </tbody>
+    </table>
+
   </div>
 </template>
 
 <script>
   import { calculateAllElo, calculateEloOverTime } from '../lib/eloCalculator';
   import {
-    createBarChartData,
-    createBarChart,
     createLineChartData,
     createLineChart
   } from '../lib/charts';
@@ -20,15 +33,18 @@
       ruleset: String
     },
     data: function() {
-      return {}
+      return {
+        infos: []
+      }
     },
     mounted: function() {
       fetch(`data/${this.ruleset}.txt`)
         .then(response => response.text())
         .then(txt => {
-          const allElo = calculateAllElo(txt);
-          const allEloData = createBarChartData(allElo);
-          createBarChart(this.ruleset, allEloData);
+          const people = calculateAllElo(txt);
+          this.infos = Object.keys(people)
+            .map(person => ({ person, elo: people[person].toFixed(1) }))
+            .sort((a, b) => b.elo - a.elo);
 
           const eloOverTime = calculateEloOverTime(txt);
           const eloOverTimeData = createLineChartData(eloOverTime)
