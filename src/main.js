753b26109ce480aaf2f177424897187fd8cc4fe3
turtleman270 <ahill5067@gmail.com>
2019-05-26 00:01:43 -0400

    .

--- src/main.js
+++ src/main.js
@@ -9,10 +9,16 @@ new Vue({
   render: h => h(App),
 }).$mount('#app')
 /* eslint-disable */
-console.log(fetch("/slack/T025EV233-FJZB1MFT9/fiveminutespluszeroseconds.txt").response())
+console.log(fetch("/slackfile/").response())
 console.log("break")
-fetch("/slack/T025EV233-FJZB1MFT9/fiveminutespluszeroseconds.txt")
+fetch("/slackfile/")
   .then(response => response.text())
   .then(txt => {
     console.log(txt)
-  });
+  });console.log(fetch("/slack/T025EV233-FJZB1MFT9/fiveminutespluszeroseconds.txt").response())
+  console.log("break")
+  fetch("/slack/T025EV233-FJZB1MFT9/fiveminutespluszeroseconds.txt")
+    .then(response => response.text())
+    .then(txt => {
+      console.log(txt)
+    });
